0344b443b994160b9f1990c08580048ee75be983
4centto <ivangarcia080902@gmail.com>
2023-09-05 16:13:19 -0600

    Tarea 5

---
+++ Tarea5_CastroGarciaIvan/app3.js
@@ -0,0 +1,19 @@
+var nombre = "Ivan"
+var nombre = "Marianna"
+
+console.log(nombre)
+
+//Comentarios
+let user = "4centto"
+
+console.log(user)
+
+let uno = 5
+let dos = 12
+
+//Operaciones
+
+console.log(uno + dos)
+console.log(uno - dos)
+console.log(uno * dos)
+console.log(uno / dos)
---
+++ Tarea5_CastroGarciaIvan/app4.js
@@ -0,0 +1,15 @@
+console.log("Nombre es: ")
+let nombreUser = "Ivan Castro Garcia"
+console.log(nombreUser)
+
+console.log("Edad es: ")
+let edad = 20
+console.log(edad)
+
+console.log("Nacionalidad es: ")
+let nacionalidad = "Mexicana"
+console.log(nacionalidad)
+
+console.log("Estas comprometido? ")
+let comprometido = "No"
+console.log(comprometido)
